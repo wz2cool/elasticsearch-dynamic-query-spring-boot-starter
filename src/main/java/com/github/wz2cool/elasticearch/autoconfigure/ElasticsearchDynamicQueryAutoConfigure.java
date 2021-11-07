8837f8b515c60e31301ef97edd4b3ea0c61f9bf6
wz2cool <wz2cool@live.cn>
2021-11-07 10:34:22 +0800

    init

---
+++ src/main/java/com/github/wz2cool/elasticearch/autoconfigure/ElasticsearchDynamicQueryAutoConfigure.java
@@ -0,0 +1,12 @@
+package com.github.wz2cool.elasticearch.autoconfigure;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Frank
+ **/
+@Configuration
+public class ElasticsearchDynamicQueryAutoConfigure {
+
+
+}
